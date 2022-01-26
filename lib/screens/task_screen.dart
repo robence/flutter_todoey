8ed14c7e6809b3739b9cc38093b9cec7e6bd27c6
Bence Rozsa <robence@gmail.com>
2022-01-26 14:17:42 +0100

    create task layout

--- lib/screens/task_screen.dart
+++ lib/screens/task_screen.dart
@@ -7,7 +7,57 @@ class TaskScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lightBlue,
-      body: Container(),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: const EdgeInsets.only(
+                top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                CircleAvatar(
+                  radius: 30.0,
+                  backgroundColor: Colors.white,
+                  child: Icon(
+                    Icons.list,
+                    color: Colors.lightBlue,
+                    size: 30.0,
+                  ),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text(
+                  'Todoey',
+                  style: TextStyle(
+                      fontSize: 50,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w700),
+                ),
+                Text(
+                  '12 Tasks',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20.0),
+                  topRight: Radius.circular(20.0),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
